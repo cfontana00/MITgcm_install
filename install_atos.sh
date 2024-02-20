7584533f4c97df4a63e22279cd6c3563fbe6857e
cfontana00 <clementfontana01@gmail.com>
2024-02-20 13:31:41 +0000

    architecture

---
+++ install_atos.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+
+source parameters.sh
+INST=$PWD
+cd $LOC
+
+
+MIT_HOME=$LOC/NWMed
+
+git clone git@github.com:alessioinnocenti/NWMed.git
+cd NWMed
+git checkout atos
+
+
+
+user=`whoami`
+mkdir -p /ec/res4/scratch/$user/MITgcm_BFM/
+mkdir -p /ec/res4/scratch/$user/MITgcm_BFM/WORK_benchmark
+
+pwd
+cd NWMed
+
+cat $INST/chain/chain_env.sh | sed -e s_%%PATH%%_${MIT_HOME}_g \
+            | sed -e s_%%USER%%_${user}_g > chain_env.sh
+source chain_env.sh
+
+
+# Edit files
+# ----------
+cd bin/src
+cp $INST/chain/mit_profile__atos.src_inc .
+cp $INST/chain/mit-compiler.ksh CONFIG
+cp $INST/chain/Makefile .
+
+
+for file in `ls *src_ksh`;do
+  cat $file | sed -e s/itai/$user/g > tmp
+  mv tmp $file
+done
+
+make
+make install
+
+./mit_setup_directories.ksh
+
+cd ../../
+
+
+for file in `ls *.sh`;do
+  cat $file | sed -e s/itai/$user/g > tmp
+  mv tmp $file
+  chmod u+x $file
+done
+
+
+
+
